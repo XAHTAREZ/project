b2701d07411cb866291c0aba7cc045dae1f65d89
Denis <barovdenis@mail.ru>
2022-02-14 15:32:44 +0300

    Задания

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
+// Первое задание:
+
 let numberOfFilms;
 
-numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
-console.log(numberOfFilms);
+// Второе задание
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+// Третье задание
+
+let lookFilm;
+let filmRating;
+
+lookFilm = prompt("Один из просмотренных фильмов?");
+filmRating = prompt("На сколько оцените его?");
+
+personalMovieDB.movies = `${lookFilm} : ${filmRating}`;
+
+console.log(personalMovieDB);
