4eee24739b31f01cd654560cc5f5e682e2bc5d84
Denis <barovdenis@mail.ru>
2022-02-15 15:47:32 +0300

    Измененный js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,10 @@
 "use strict";
 
-const str = "test";
-const arr=[1, 2, 4];
+function test (num, callback) {
+   console.log(num);
+   callback();
+}
 
-console.log(arr.length);
-
-const text = "Barov Denis";
-
-console.log(text.slice(0, 5));
-
-console.log(text.substring(0, 5));
-
-console.log(text.substr(6, 5));
-
-const num = 12.2;
-
-console.log(Math.round(num));
-
-const test = "12.2px";
-
-console.log(parseInt(test));
+test('5', function () {
+   console.log('Следующая');
+});
